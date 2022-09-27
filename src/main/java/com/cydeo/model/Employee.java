e10375ca84d6d060e3146b5a4280d9925ccc40b5
Nmerkel <noramerkel0@gmail.com>
2022-09-27 13:09:44 -0400

    working on to save employee info

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,6 +1,7 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -14,7 +15,11 @@ public class Employee {
 
     private String firstName;
     private String lastName;
+
+    //Thymeleaf returns yyyy-mm-dd, but LocalDate accepts mm-dd-yyyy, we have to use annotation to fix the changes
+    @DateTimeFormat(pattern="yyyy-mm-dd")
     private LocalDate birthday;
+
     private String email;
     private String password;
     private String address;
