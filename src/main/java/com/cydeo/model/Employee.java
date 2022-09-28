9e19ffc51bd7a3dac283d9ad3be1f54c0a7182f3
Nmerkel <noramerkel0@gmail.com>
2022-09-27 21:28:01 -0400

    How to avoid incorrect entries from going through using annoation of @NoNull @NoEmpty @NoBlank

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,6 +16,14 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+  //  @NotNull ---> field shouldn't be null'
+  //  @NotEmpty ---> field shouldn't be an empty string ""
+   // @NotBlank ---> filed shouldn't be blank "           "
+    // @NotNull->@NotNull
+    //@NotEmpty->@NotNull +@NotEmpty
+    //@NotBlank->@NotNull+@NullEmpty+@NotBlank
+    @NotBlank
+    @Size(max=12, min=2)
     private String firstName;
     private String lastName;
 
